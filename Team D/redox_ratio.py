89640a85767f909dd05c7d2460877edf6655eae5
bcjojo <giberson@uark.edu>
2023-11-14 15:41:34 -0600

    adding model file for team D

--- Team D/redox_ratio.py
+++ Team D/redox_ratio.py
@@ -9,26 +9,41 @@ Model()
 Monomer("NADH", ['b'])
 Monomer("Ox", ['b'])
 Monomer("NAD_plus", ['b'])
+Monomer("NADH_Ox", ['b'])  # NADH-oxidase complex
+Monomer("NAD_plus_Ox", ['b'])  # NAD+-oxidase complex
 
 # Initial conditions (concentrations of species at the start)
 Parameter("NADH_init", 100)
 Parameter("Ox_init", 50)
 Parameter("NAD_plus_init", 0)  # Assuming initially there's no NAD+
+Parameter("NADH_Ox_init", 0)  # Assuming initially there's no NADH-Ox complex
+Parameter("NAD_plus_Ox_init", 0)  # Assuming initially there's no NAD+-Ox complex
 Initial(NADH(b=None), NADH_init)
 Initial(Ox(b=None), Ox_init)
 Initial(NAD_plus(b=None), NAD_plus_init)
+Initial(NADH_Ox(b=None), NADH_Ox_init)
+Initial(NAD_plus_Ox(b=None), NAD_plus_Ox_init)
+
 
 # Reaction rates
-Parameter("kp1", .01)  # Forward rate for NADH binding to Ox
-Parameter("km1", 1)    # Reverse rate for NADH unbinding from Ox
-Parameter("kp2", .01)  # Forward rate for NADH_Ox converting to NAD+
-Parameter("km2", 1)    # Reverse rate for NAD+ binding to Ox
+Parameter("kp1", .01)
+Parameter("km1", 1)
+Parameter("kp2", .01)
+Parameter("km2", 1)
+# Additional rates for generalized kinetics
+Parameter("r_plus_oxi", .01)
+Parameter("r_minus_oxi", 1)
+
 
 # Reaction rules
 # NADH binding to Ox (reversible)
 Rule("NADH_binds_Ox", NADH(b=None) + Ox(b=None) | NADH(b=1) % Ox(b=1), kp1, km1)
 # NADH_Ox converting to NAD+ and free Ox (reversible)
 Rule("NADH_Ox_to_NAD_plus", NADH(b=1) % Ox(b=1) | NAD_plus(b=None) + Ox(b=None), kp2, km2)
+
+# Additional rules for generalized kinetics
+Rule("NADH_Ox_to_NAD_plus_Ox", NADH_Ox(b=None) >> NAD_plus_Ox(b=None), r_plus_oxi)
+Rule("NAD_plus_Ox_to_NADH_Ox", NAD_plus_Ox(b=None) >> NADH_Ox(b=None), r_minus_oxi)
 
 # Observables (for tracking concentration changes over time)
 Observable("NADH_free", NADH(b=None))
