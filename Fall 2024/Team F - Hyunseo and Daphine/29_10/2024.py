137f136a684e5438662b89ac43c3e95f7b9c835a
dakot <dakot@uark.edu>
2024-10-31 16:54:28 -0500

    Notes(D)

--- Fall 2024/Team F - Hyunseo and Daphine/29_10/2024.py
+++ Fall 2024/Team F - Hyunseo and Daphine/29_10/2024.py
@@ -16,22 +16,37 @@ Initial(A(b=None),A_0)
 Initial(B(a=None, state='u'),B_0)
 
 #Rules
-Parameter('kf_AB', 100)
-Parameter('kr_AB', 80)
-Rule('A_B_binds', A(b=None) + B(a=None,state='u') | A(b=1) % B(a=1,state='u'), kf_AB, kr_AB)
+Parameter('kf_AB', 1)
+Parameter('kr_AB', 10)
+Rule('A_B_binds', A(b=None) + B(a=None, state='u') | A(b=1) % B(a=1, state='u'), kf_AB, kr_AB)
 
-Parameter('k_B_phos',0.1)
-Rule('AB_phosphorylate', A(b=1) % B(a=1),state='u') >> A(b=1) % B(a=1,state='p'), k_B_phos)
+Parameter('k_B_phos',10)
+Rule('AB_phosphorylate', A(b=1) % B(a=1, state='u') >> A(b=1) % B(a=1,state='p'), k_B_phos)
 
 Parameter('k_ABp_unbind',1)
-Rule('A_Bp_unbinds', A(b=1) % B(a=1),state='p') >> A(b=None) + B(a=None,state='p', k_ABp_unbinds)
+Rule('A_Bp_unbinds', A(b=1) % B(a=1,state='p') >> A(b=None) + B(a=None,state='p'), k_ABp_unbind)
 
 Parameter('k_Bp_unphos',10)
-Rule('Bp_unphos',B(a=None),state='p') >> B(a=None,state='u', k_Bp_unphos)
-#Observables
+Rule('Bp_unphos',B(a=None,state='p') >> B(a=None,state='u'), k_Bp_unphos)
 
+#Observables
+Observable('B_u', B(state='u'))
+Observable('B_p', B(state='p'))
+Observable('Free_A', A(b=None))
+Observable('Free_B', B(a=None))
+Observable('AB_complex', A(b=1) % B(a=1))
+Observable('ABp_complex', A(b=1) % B(a=1,state='p'))
 
 #Simulation commands + plotting
+tspan= np.linspace(0,1,101)
+sim=ScipyOdeSimulator(model,tspan,initials=True)
+result= sim.run()
 
+for obs in model.observables:
+    plt.plot(tspan, result.observables(obs.name), lw=2, label=obs.name)
+    plt.xlabel('time')
+    plt.ylabel('concentration')
+    plt.legend(loc=0,ncol=2)
+    plt.tight_layout()
 
-
+    plt.show()
