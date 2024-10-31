2fe57aa2f7058c6f2fb186da79d192abeff89607
Abbey Bryan <abbeyb@uark.edu>
2024-10-31 16:50:45 -0500

    Finished the observables and plotting for the practice for pycharm. Code is working and producing a graph.

--- Fall 2024/Team E - Abbey and Divya/Abbey_practice1.py
+++ Fall 2024/Team E - Abbey and Divya/Abbey_practice1.py
@@ -22,16 +22,37 @@ Initial(B(a=None, state='u'), B_0)
 Parameter('kf_AB', 1)
 Parameter('kr_AB', 10)
 Parameter('k_B_phos', 0.1)
-Parameter(' k_ABp_unbind', 1)
+Parameter('k_ABp_unbind', 1)
 Parameter( 'k_Bp_unphos', 10)
-Rule('A_B_binds', A(b=None), B(a=None,state='u') | A(b=1) % B(a=1, state='u'), kf_AB, kr_AB)
+Rule('A_B_binds', A(b=None) + B(a=None,state='u') | A(b=1) % B(a=1, state='u'), kf_AB, kr_AB)
 Rule('AB_phos', A(b=1) % B(a=1, state='u') >> A(b=1) % B(a=1, state='p'), k_B_phos)
 Rule('AB_unbinds', A(b=1) % B(a=1, state='p') >> A(b=None) + B(a=None, state='p'), k_ABp_unbind)
 Rule('Bp_unphos', B(a=None, state = 'p') >> B(a=None, state = 'u'), k_Bp_unphos)
 
 # Observables
 
-
-
+Observable('B_p', B(state = 'p'))
+Observable('Free_A', A(b=None))
+Observable('Free_B', B(a=None))
+Observable('AB_complex', A(b=1) % B(a=1))
+Observable('ABp_complex',A(b=1) % B(a=1, state='p'))
 
 # Simulation Commands
+
+tspan = np.linspace(0, 1, 100)
+sim = ScipyOdeSimulator(model, tspan, verbose=True)
+result = sim.run()
+
+for obs in model.observables:
+    plt.plot(tspan, result.observables[obs.name], lw=2, label=obs.name)
+
+plt.xlabel('time')
+plt.ylabel('concentration')
+plt.legend(loc=0)
+
+plt.show()
+
+
+
+
+
