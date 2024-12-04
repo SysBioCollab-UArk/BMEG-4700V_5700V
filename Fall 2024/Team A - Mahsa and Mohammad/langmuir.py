069fde19024fed3bb237517c4f17868984d4a3af
mahsakhalili5 <mkhalili@uark.edu>
2024-12-03 19:40:28 -0600

    Dec 3,24

--- Fall 2024/Team A - Mahsa and Mohammad/langmuir.py
+++ Fall 2024/Team A - Mahsa and Mohammad/langmuir.py
@@ -26,14 +26,16 @@ Rule('protein_binds_surface', P(b=None) + S(b=None) | P(b=1) % S(b=1), ka, kd)
 
 Observable('free_protein', P(b=None))
 Observable('free_surface_sites', S(b=None))
-Observable('bound_surface_sites', P(b=1) % S(b=1))
+Observable('bound_surface_sites', S(b=ANY))
+Observable('total_surface_sites', S())
+Expression('Theta', bound_surface_sites / total_surface_sites )
 
 if __name__ == '__main__':
 
     # simulations
-    PLOT_TIMECOURSES = False
+    PLOT_TIMECOURSES = True
     fraction_sites_bound = []
-    ph_vals = np.arange(6, 10.1, 0.2)
+    ph_vals = np.arange(6, 10.1, 0.5)
     for ph in ph_vals:
         print('pH: %g' % ph)
 
@@ -41,15 +43,15 @@ if __name__ == '__main__':
         sim = ScipyOdeSimulator(model, tspan, verbose=False)
         output = sim.run(param_values={'pH': ph})
 
-        fraction_sites_bound.append(output.observables['bound_surface_sites'][-1] / S_init.value)
+        fraction_sites_bound.append(output.expressions['Theta'][-1])
 
         if PLOT_TIMECOURSES:
-            plt.figure(constrained_layout=True)
-            plt.title('pH = %g' % ph)
-            for obs in model.observables:
-                plt.plot(tspan, output.observables[obs.name], lw=2, label=obs.name)
+            #plt.figure(constrained_layout=True)
+            #plt.title('pH = %g' % ph)
+            plt.plot(tspan, output.expressions['Theta'], lw=2, label= 'pH = %g' % ph)
             plt.xlabel('time')
-            plt.ylabel('amount')
+            plt.ylabel('Theta')
+            plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
             plt.legend(loc='best')
 
     plt.figure(constrained_layout=True)
