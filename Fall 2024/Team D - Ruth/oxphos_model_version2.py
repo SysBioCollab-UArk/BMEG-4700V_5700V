08819cfea32cb11f2aab5bd920c0d19fe606fd91
ruthc <skydreamx7@yahoo.co.uk>
2024-12-16 12:46:22 -0600

    Final version - complex I & II only

--- Fall 2024/Team D - Ruth/oxphos_model_version2.py
+++ Fall 2024/Team D - Ruth/oxphos_model_version2.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 # FAD/(FAD + NAD(P)H) ratio
 # initial FAD amount
 # How to make RET conditional?
+# ADP & Pi as substrates
+# Can the model be used to create an equation relating the redox ratio and ROS?
 
 Model()
 
@@ -36,17 +38,16 @@ Monomer('Fumarate')
 # Initial conditions
 Parameter('CI_0', 100)
 Parameter('NADH_0', 1e4)
-Parameter('Q_CI_0', 100)
-Parameter('Q_CII_0', 100)
 Parameter('O2_0', 100)
 Parameter('CII_0', 100)
-Parameter('SUCC_0', 1e3)
+Parameter('SUCC_0', 100)
 
 Initial(Complex_I(fmn=1, q=2) % FMN(e_='_0', cI=1) % Q_Complex_I(e_='_0', cI=2), CI_0)
 Initial(NADH(), NADH_0)
 Initial(O2(), O2_0)
 Initial(Complex_II(fad=1, q=2) % FAD(e_='_0', cII=1) % Q_Complex_II(e_='_0', cII=2), CII_0)
 Initial(Succinate(), SUCC_0)
+
 
 
 
@@ -113,13 +114,26 @@ Rule('CII_Q_reduction_QH2',
 
 
 # Observables
+# Complex I
 Observable('FMN_e0', FMN(e_='_0'))
 Observable('FMN_e1', FMN(e_='_1'))
 Observable('FMN_e2', FMN(e_='_2'))
-Observable('Q_e0', Q_Complex_I(e_='_0'))
-Observable('Q_e1', Q_Complex_I(e_='_1'))
-Observable('Q_e2', Q_Complex_I(e_='_2'))
-Observable('QH2_tot', QH2_Complex_I())
+Observable('QI_e0', Q_Complex_I(e_='_0'))
+Observable('QI_e1', Q_Complex_I(e_='_1'))
+Observable('QI_e2', Q_Complex_I(e_='_2'))
+Observable('QIH2_tot', QH2_Complex_I())
+
+# Complex II
+Observable('Succinate_to_fumarate', Succinate())
+Observable('FAD_e0', FAD(e_='_0'))
+Observable('FAD_e1', FAD(e_='_1'))
+Observable('FAD_e2', FAD(e_='_2'))
+Observable('QII_e0', Q_Complex_II(e_='_0'))
+Observable('QII_e1', Q_Complex_II(e_='_1'))
+Observable('QII_e2', Q_Complex_II(e_='_2'))
+Observable('QIIH2_tot', QH2_Complex_II())
+
+# ROS generation
 Observable('O2_tot', O2())
 Observable('SuperOx', O2_())
 
@@ -134,15 +148,24 @@ for obs in model.observables:
     plt.plot(tspan, output.observables[obs.name], lw=2, label=obs.name)
 plt.xlabel('Time')
 plt.ylabel('Concentration')
-plt.legend(loc='best')
+plt.legend(loc=(0.6, 0.05))
 plt.tight_layout()
 
-'''
-for obs in [o for o in model.observables if o.name not in ['O2_tot', 'SuperOx']]:
+plt.figure()
+for obs in [o for o in model.observables if o.name not in ['O2_tot', 'SuperOx', 'Succinate_to_fumarate', 'FAD_e0', 'FAD_e1',
+                                                           'FAD_e2', 'QII_e0', 'QII_e1', 'QII_e2', 'QIIH2_tot']]:
     plt.plot(tspan, output.observables[obs.name], lw=2, label=obs.name)
 plt.xlabel('Time')
 plt.ylabel('Concentration')
 plt.legend(loc=(0.5,0.55), ncol=2)
+plt.tight_layout()
+
+plt.figure()
+for obs in [o for o in model.observables if o.name not in ['O2_tot', 'SuperOx', 'FMN_e0', 'FMN_e1', 'FMN_e2', 'QI_e0', 'QI_e1', 'QI_e2', 'QIH2_tot']]:
+    plt.plot(tspan, output.observables[obs.name], lw=2, label=obs.name)
+plt.xlabel('Time')
+plt.ylabel('Concentration')
+plt.legend(loc='best')
 plt.tight_layout()
 
 plt.figure()
@@ -152,5 +175,5 @@ plt.xlabel('Time')
 plt.ylabel('Concentration')
 plt.legend(loc='best')
 plt.tight_layout()
-'''
+
 plt.show()
